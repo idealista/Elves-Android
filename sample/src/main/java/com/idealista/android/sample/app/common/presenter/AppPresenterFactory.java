7ddf1b351198d7d844a01b0f841018cffa48ccaf
Nicolás Patarino <npatarino@gmail.com>
2017-01-16 16:16:54 +0100

    Move presenterFactory and navigatorFactory

--- sample/src/main/java/com/idealista/android/sample/app/common/presenter/AppPresenterFactory.java
+++ sample/src/main/java/com/idealista/android/sample/app/common/presenter/AppPresenterFactory.java
@@ -5,6 +5,8 @@ import com.idealista.android.elvesandroid.navigator.view.PresenterFactory;
 import com.idealista.android.mvp.NullPresenter;
 import com.idealista.android.mvp.Presenter;
 import com.idealista.android.mvp.View;
+import com.idealista.android.sample.app.movie.presenter.MoviePresenter;
+import com.idealista.android.sample.app.movie.view.MovieView;
 import com.idealista.android.sample.app.movies.presenter.MoviesPresenter;
 import com.idealista.android.sample.app.movies.view.MoviesView;
 
@@ -20,6 +22,9 @@ public class AppPresenterFactory<P extends Presenter> implements PresenterFactor
         if (view instanceof MoviesView) {
             MoviesView castedView = (MoviesView) view;
             presenter = new MoviesPresenter(castedView);
+        } else if (view instanceof MovieView) {
+            MovieView castedView = (MovieView) view;
+            presenter = new MoviePresenter(castedView);
         } else {
             presenter = new NullPresenter(view);
         }
