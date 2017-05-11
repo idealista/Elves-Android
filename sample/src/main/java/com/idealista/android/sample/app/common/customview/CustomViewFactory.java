ccfc81fc00c8a7c83f5d57fe410a34cd9c99f5c5
Jose del Pozo <jmdelpozo@idealista.com>
2017-05-11 16:15:56 +0200

    Added footer and header features to Adapter Updated support version to 25.3.1

--- sample/src/main/java/com/idealista/android/sample/app/common/customview/CustomViewFactory.java
+++ sample/src/main/java/com/idealista/android/sample/app/common/customview/CustomViewFactory.java
@@ -7,6 +7,9 @@ import android.support.annotation.NonNull;
 import com.idealista.android.elves.view.mvp.view.CustomViewCreator;
 import com.idealista.android.elves.view.widget.CustomView;
 import com.idealista.android.sample.app.model.MovieModel;
+import com.idealista.android.sample.app.movies.view.FooterMovieView;
+import com.idealista.android.sample.app.movies.view.HeaderMovieView;
+import com.idealista.android.sample.app.movies.view.LoadingView;
 import com.idealista.android.sample.app.movies.view.MovieView;
 
 public final class CustomViewFactory {
---
+++ sample/src/main/java/com/idealista/android/sample/app/movies/view/HeaderMovieView.java
@@ -0,0 +1,58 @@
+package com.idealista.android.sample.app.movies.view;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+import com.idealista.android.elves.view.widget.LinearView;
+import com.idealista.android.elves.view.widget.OnClicked;
+import com.idealista.android.sample.R;
+
+public class HeaderMovieView extends LinearView<Void> {
+
+    private TextView textViewTitle;
+    private OnClicked<Void> onClicked;
+
+    public HeaderMovieView(Context context) {
+        super(context);
+    }
+
+    public HeaderMovieView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HeaderMovieView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public HeaderMovieView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public void render(@NonNull final Void aVoid) {
+    }
+
+    @Override
+    public void prepare() {
+        textViewTitle = (TextView) findViewById(R.id.tvTitle);
+        textViewTitle.setText("Header ** Idealista **");
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.view_movie;
+    }
+
+    @Override
+    public void setOnClicked(@NonNull OnClicked<Void> onClicked) {
+        this.onClicked = onClicked;
+    }
+
+
+}
